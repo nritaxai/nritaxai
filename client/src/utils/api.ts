89a6552705ba2315e0da3cf93a1383a57a202e1a
Hema Varshini <hemas.nritaxai@gmail.com>
2026-02-13 20:49:45 +0530

    added render backend deployement

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://nritaxai-cfbl.vercel.app/"; // replace with your backend URL
+const API_URL = "https://nritaxai.onrender.com"; // replace with your backend URL
 
 export const signupUser = async (signupData: any) => {
   const response = await axios.post(`${API_URL}/api/auth/register`, signupData);
