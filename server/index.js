ff32955488aff5ccf9d57497ef52ab35991b329e
Hema Varshini <hemas.nritaxai@gmail.com>
2026-02-13 20:58:39 +0530

    fixed cors issue

--- server/index.js
+++ server/index.js
@@ -15,10 +15,23 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(express.json());
-app.use(cors({
-  origin: ["http://localhost:5173", "https://nritaxai.vercel.app"],
-  credentials: true,
-}));
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://nritaxai.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+// Handle preflight requests
+// app.options("*", cors());
+
+// app.use(cors({
+//   origin: ["http://localhost:5173", "https://nritaxai.vercel.app"],
+//   credentials: true,
+// }));
 
 // Routes
 app.use("/api/auth", authRoute);
